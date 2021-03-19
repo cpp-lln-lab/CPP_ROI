33a99c3299f203f50b5efeb0b144746eb276b93e
Remi Gau <remi_gau@hotmail.com>
2021-03-19 20:18:45 +0100

    improve initCppRoi

--- initCppRoi.m
+++ initCppRoi.m
@@ -1,12 +1,23 @@
 % (C) Copyright 2021 CPP ROI developers
 
 function initCppRoi()
-
-  % directory with this script becomes the current directory
-  WD = fileparts(mfilename('fullpath'));
-
-  % we add all the subfunctions that are in the sub directories
-  addpath(genpath(fullfile(WD, 'src')));
-  addpath(genpath(fullfile(WD, 'lib', 'marsbar-0.44')));
-
+    
+    global CPP_ROI_INITIALIZED
+    
+    if isempty(CPP_ROI_INITIALIZED)
+        
+        % directory with this script becomes the current directory
+        WD = fileparts(mfilename('fullpath'));
+        
+        % we add all the subfunctions that are in the sub directories
+        addpath(genpath(fullfile(WD, 'src')));
+        addpath(fullfile(WD, 'lib', 'marsbar-0.44'));
+        
+        CPP_ROI_INITIALIZED = true();
+        
+    else
+        fprintf('\n\nCPP_ROI already initialized\n\n');
+        
+    end
+    
 end
