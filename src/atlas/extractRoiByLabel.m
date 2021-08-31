3d42d03698ec97e7f684d56bb685e38baef4eb4d
Remi Gau <remi_gau@hotmail.com>
2021-08-31 14:08:35 +0200

    add tests and update CI and rename demos

--- src/atlas/extractRoiByLabel.m
+++ src/atlas/extractRoiByLabel.m
@@ -1,6 +1,20 @@
-% (C) Copyright 2021 CPP ROI developers
-
 function outputImage = extractRoiByLabel(sourceImage, labelStruct)
+  %
+  % Given a discrete segmentation source image fullpath and a look up table
+  % label structure this creates a mask for that ROI and returns the fullpath to its BIDS name.
+  % This will also create a JSON side car file for that image.
+  %
+  % USAGE::
+  %
+  %   outputImage = extractRoiByLabel(sourceImage, labelStruct)
+  %
+  % :param sourceImage: discrete segmentation source image fullpath
+  % :type sourceImage: string
+  % :param labelStruct: 1x1 structure with fields ``ROI`` for the ROI name
+  %                     and ``label`` for the corresponding label
+  % :type labelStruct: structure
+  %
+  % (C) Copyright 2021 CPP ROI developers
 
   hdr = spm_vol(sourceImage);
   vol = spm_read_vols(hdr);
@@ -9,14 +23,13 @@ function outputImage = extractRoiByLabel(sourceImage, labelStruct)
   outputVol(vol == labelStruct.label) = true;
 
   p = bids.internal.parse_filename(sourceImage);
-  p.label = labelStruct.ROI;
+  p.entities.label = labelStruct.ROI;
   p.suffix = 'mask';
   p.use_schema = false;
 
   newName = bids.create_filename(p);
   hdr.fname = spm_file(hdr.fname, 'filename', newName);
 
-  % Cluster labels as their size.
   spm_write_vol(hdr, outputVol);
   outputImage = hdr.fname;
 
--- src/roi/keepHemisphere.m
+++ src/roi/keepHemisphere.m
@@ -9,7 +9,7 @@ function outputImage = keepHemisphere(inputImage, hemisphere)
   %
   % :param image:
   % :type image: string
-  % :param hemisphere: ``'lh'`` or ``'rh'``
+  % :param hemisphere: ``'L'`` or ``'R'``
   % :type hemisphere: string
   %
   %
@@ -23,19 +23,23 @@ function outputImage = keepHemisphere(inputImage, hemisphere)
   xDim = hdr.dim(1);
   xMid = round(xDim / 2);
 
-  switch lower(hemisphere)
+  switch hemisphere
 
-    case 'lh'
+    case 'L'
       discard = 1:xMid;
 
-    case 'rh'
+    case 'R'
       discard = xMid:xDim;
+
+    otherwise
+      error('hemisphere must be L or R.');
+
   end
 
   vol(discard, :, :) = NaN;
 
   p = bids.internal.parse_filename(inputImage);
-  p.entities.hs = lower(hemisphere);
+  p.entities.hemi = hemisphere;
   p.use_schema = false;
   newName = bids.create_filename(p);
 
