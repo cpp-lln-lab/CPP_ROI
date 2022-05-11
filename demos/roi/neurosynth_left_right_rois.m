a1d719f762e737c911b2289234e614de91ac7868
Remi Gau <remi_gau@hotmail.com>
2022-05-11 20:27:32 +0200

    fix demos

--- demos/roi/neurosynth_left_right_rois.m
+++ demos/roi/neurosynth_left_right_rois.m
@@ -6,7 +6,7 @@
 run ../../initCppRoi;
 
 gunzip(fullfile('inputs', '*.gz'));
-zMap = fullfile(pwd, 'inputs', 'visual_motion_association-test_z_FDR_0.01.nii');
+zMap = fullfile(pwd, 'inputs', 'visual motion_association-test_z_FDR_0.01.nii');
 
 zMap = renameNeuroSynth(zMap);
 roiImage = thresholdToMask(zMap, 5);
