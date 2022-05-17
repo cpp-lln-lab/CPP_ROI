47918f67cc342cadf35775611ed98a2c63ce126a
Remi Gau <remi_gau@hotmail.com>
2022-05-17 19:44:39 +0200

    fix another test that was failing because of a bids matlab update

--- tests/test_createRoi.m
+++ tests/test_createRoi.m
@@ -49,7 +49,7 @@ function test_createRoi_intersection_mask_sphere()
 
   mask = createRoi('intersection', specification, volumeDefiningImage, outputDir, saveImg);
 
-  basename = 'rspace-MNI_atlas-neurosynth_label-visualMotionIntersection_desc-p10pt00_mask';
+  basename = 'space-MNI_atlas-neurosynth_label-visualMotionIntersection_desc-p10pt00_mask';
 
   assertEqual(exist(fullfile(thisDir(), [basename '.nii']), 'file'), 2);
   assertEqual(exist(fullfile(thisDir(), [basename '.json']), 'file'), 2);
@@ -75,7 +75,7 @@ function test_createRoi_expand
 
   mask = createRoi('expand', specification, volumeDefiningImage, outputDir, saveImg);
 
-  basename = 'rspace-MNI_atlas-neurosynth_label-visualMotionExpandVox57_desc-p10pt00_mask';
+  basename = 'space-MNI_atlas-neurosynth_label-visualMotionExpandVox57_desc-p10pt00_mask';
 
   assertEqual(mask.roi.size, 57);
 
@@ -95,7 +95,8 @@ function value = demoDir()
 
   value = fullfile(thisDir(), '..', 'demos', 'roi', 'inputs');
 
-  if exist(fullfile(value, 'TStatistic.nii'), 'file') == 0
+  if exist(fullfile(value, 'TStatistic.nii'), 'file') == 0 || ...
+     exist(fullfile(value, 'visual motion_association-test_z_FDR_0.01.nii'), 'file') == 0
     gunzip(fullfile(value, '*.gz'));
   end
 
@@ -106,7 +107,7 @@ function  [roiFilename, volumeDefiningImage] = prepareRoiAndVolumeDefiningImage(
   volumeDefiningImage = fullfile(demoDir(), 'TStatistic.nii');
 
   roiFilename = fullfile(demoDir(), ...
-                         'rspace-MNI_atlas-neurosynth_label-visualMotion_desc-p10pt00_mask.nii');
+                         'space-MNI_atlas-neurosynth_label-visualMotion_desc-p10pt00_mask.nii');
 
   if exist(roiFilename, 'file') == 2
 
@@ -116,6 +117,8 @@ function  [roiFilename, volumeDefiningImage] = prepareRoiAndVolumeDefiningImage(
 
     zMap = renameNeuroSynth(zMap);
     zMap = resliceRoiImages(volumeDefiningImage, zMap);
+
+    zMap = removePrefix(zMap, 'r');
 
     threshold = 10;
     roiFilename = thresholdToMask(zMap, threshold);
